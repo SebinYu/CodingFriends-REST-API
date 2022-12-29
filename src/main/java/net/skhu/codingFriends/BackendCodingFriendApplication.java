7d8c35c9d1ff25b6ac455811258f0b65a2d70ad5
SebinYu <sebinyu72@gmail.com>
2022-12-30 02:08:16 +0900

    chore: 기본환경세팅

---
+++ src/main/java/net/skhu/codingFriends/BackendCodingFriendApplication.java
@@ -0,0 +1,13 @@
+package net.skhu.codingFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCodingFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCodingFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/skhu/codingFriends/BackendCodingFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu.codingFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCodingFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
