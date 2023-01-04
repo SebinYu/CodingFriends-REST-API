1ee40cf0d75c4c00c41d946ab8661b90efa5eb8d
SebinYu <sebinyu72@gmail.com>
2023-01-04 18:26:21 +0900

    feat: 스터디그룹 추가 기능_rest api 연결

--- src/main/java/net/skhu/codingFriends/controller/StudygroupController.java
+++ src/main/java/net/skhu/codingFriends/controller/StudygroupController.java
@@ -1,9 +1,11 @@
 package net.skhu.codingFriends.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.skhu.codingFriends.entity.learningmaterial;
 import net.skhu.codingFriends.entity.studygroup;
 import net.skhu.codingFriends.repository.StudygroupRepository;
 import net.skhu.codingFriends.service.StudygroupService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
@@ -16,26 +18,45 @@ public class StudygroupController {
 
     private final StudygroupService studygroupService;
 
+    //http://localhost:8081/studygroup/list
     @GetMapping("list")
     public List<studygroup> list(){
         return studygroupService.findAll();
     }
 
+    //http://localhost:8081/studygroup/detail
     @GetMapping("detail")
     public studygroup detail(@RequestParam BigInteger studyGroup_id){
         return studygroupService.findOneStudygroupInfo(studyGroup_id);
     }
 
+    //http://localhost:8081/studygroup/search-With-Keyword?keyword=파이썬
     @GetMapping("search-With-Keyword")
     public List<studygroup> searchWithKeyword(@RequestParam String keyword){
         return studygroupService.searchWithKeyword(keyword);
     }
 
-    @GetMapping("search-With-searchWithLearningMaterial_idAndKeyword-And-Keyword")
+    //http://localhost:8081/studygroup/search-With-LearningMaterial_idAndKeyword/learningMaterial_id=1&keyword=파이썬
+    @GetMapping("search-With-LearningMaterial_idAndKeyword")
     public List<studygroup> searchWithLearningMaterial_idAndKeyword(
             @RequestParam Integer learningMaterial_id,
             @RequestParam String keyword){
 
         return studygroupService.searchWithLearningMaterial_idAndKeyword(learningMaterial_id, keyword);
     }
+
+    //http://localhost:8081/studygroup/create
+    @GetMapping("create")
+    public List<learningmaterial> createGet() {
+        return studygroupService.findAllLearningMaterial();
+    }
+
+    //http://localhost:8081/studygroup/create
+    @PostMapping("create")
+    public studygroup createPost(@RequestBody studygroup studygroupInfo) {
+        //updateDate 경우 직접 timestamp 값 넣어주기
+        return studygroupService.postStudygroup(studygroupInfo);
+    }
+
+
 }
--- src/main/java/net/skhu/codingFriends/service/impl/StudygroupServiceImpl.java
+++ src/main/java/net/skhu/codingFriends/service/impl/StudygroupServiceImpl.java
@@ -1,21 +1,24 @@
 package net.skhu.codingFriends.service.impl;
 
+import net.skhu.codingFriends.entity.learningmaterial;
 import net.skhu.codingFriends.entity.studygroup;
+import net.skhu.codingFriends.repository.LearningmaterialRepository;
 import net.skhu.codingFriends.repository.StudygroupRepository;
 import net.skhu.codingFriends.service.StudygroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class StudygroupServiceImpl implements StudygroupService {
 
     @Autowired
     public StudygroupRepository studygroupRepository;
+
+    @Autowired
+    public LearningmaterialRepository learningmaterialRepository;
 
     public List<studygroup> findAll(){
         return studygroupRepository.findAll();
@@ -25,9 +28,6 @@ public class StudygroupServiceImpl implements StudygroupService {
         return studygroupRepository.findByStudyGroup_id(studyGroup_id);
     }
 
-//    public List<studygroup> searchWithLearningMaterial_id(BigInteger learningMaterial_id){
-//        return studygroupRepository.searchWithLearningMaterial_id(learningMaterial_id);
-//    }
 
     public List<studygroup> searchWithKeyword(String keyword){
         return studygroupRepository.findByTitleContaining(keyword);
@@ -36,5 +36,14 @@ public class StudygroupServiceImpl implements StudygroupService {
     public List<studygroup> searchWithLearningMaterial_idAndKeyword(Integer learningMaterial_id, String keyword){
         return studygroupRepository.searchWithLearningMaterial_idAndKeyword(learningMaterial_id, keyword);
     }
+
+    public List<learningmaterial> findAllLearningMaterial(){
+        return learningmaterialRepository.findAllLearningMaterial();
+    }
+
+    public studygroup postStudygroup(studygroup studygroupInfo){
+        return studygroupRepository.save(studygroupInfo);
+    }
+
 
 }
