514633d44d771c092816b4631228db6e8f909cbe
SebinYu <sebinyu72@gmail.com>
2023-01-27 08:43:53 +0900

    feat: 이메일 알림 기능 - 설정 비밀번호 암호화

---
+++ src/main/java/net/skhu/codingFriends/config/JasyptConfig.java
@@ -0,0 +1,38 @@
+
+package net.skhu.codingFriends.config;
+
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@EnableEncryptableProperties
+@Configuration
+public class JasyptConfig {
+    @Value("${jasypt.encryptor.password}")
+    private static String KEY = System.getenv("JASYPT_PASSWORD");
+    private static final String ALGORITHM = "PBEWithMD5AndDES";
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        encryptor.setPassword(KEY); // 암호화 키
+        encryptor.setAlgorithm(ALGORITHM); // 사용 알고리즘
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
