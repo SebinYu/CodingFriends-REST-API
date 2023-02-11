0dca74990f840a7265e47ef1d04978eddb69f788
SebinYu <sebinyu72@gmail.com>
2023-02-11 22:22:08 +0900

    cicd : 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,31 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+BUILD_JAR=$(ls /home/ubuntu/codingFriends/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/codingFriends/deploy.log
 
-REPOSITORY=/home/ubuntu/app
+echo "> build 파일 복사" >> /home/ubuntu/codingFriends/deploy.log
+DEPLOY_PATH=/home/ubuntu/codingFriends/
+cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/codingFriends/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
-
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/codingFriends/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/codingFriends/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/codingFriends/deploy_err.log &
