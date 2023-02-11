f87428fe1741f9cef5a65e46372d6f81bdf23c19
SebinYu <sebinyu72@gmail.com>
2023-02-11 23:31:00 +0900

    cicd : 배포 에러 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/myapp
 cd $REPOSITORY
 
-APP_NAME=airbnb
+APP_NAME=codingFriends
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
