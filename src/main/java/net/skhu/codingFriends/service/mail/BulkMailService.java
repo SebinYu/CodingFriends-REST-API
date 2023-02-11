2df1d685611910c5e18f29c28cd9e465b3a15967
SebinYu <sebinyu72@gmail.com>
2023-02-11 19:34:11 +0900

    feat : CompletableFuture 비동기 처리 - 리턴값 지정

--- src/main/java/net/skhu/codingFriends/service/mail/BulkMailService.java
+++ src/main/java/net/skhu/codingFriends/service/mail/BulkMailService.java
@@ -2,22 +2,18 @@ package net.skhu.codingFriends.service.mail;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.skhu.codingFriends.DesignPattern.Strategy.mailMessage.MailInfo;
-import net.skhu.codingFriends.entity.studygroup;
 import net.skhu.codingFriends.entity.user;
-import net.skhu.codingFriends.exception.studygroup.StudygroupIdNotFound;
 import net.skhu.codingFriends.repository.UserRepository;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigInteger;
-import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,27 +27,32 @@ public class BulkMailService {
 
     // api 만들기 (0)
     //쿼리 적용 메일전송 메서드 (0)
-    // @Scheduled/@Async 적용 (0)
     @Async
-    @Scheduled(fixedRate=100000000) // 단위: ms
-    @Transactional(readOnly = true)
-    public void sendMailToNoticeEventBulkUsers() {
-
-
-        List<user> users = userRepository.findByAddressLike("서울");
+    @Scheduled(fixedRate = 10000) // 단위: ms
+    public CompletableFuture<String> sendMailToNoticeEventBulkUsers() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> msFuture = CompletableFuture.supplyAsync(() -> {
+            List<user> users = userRepository.findByAddressLike("서울");
 
             SimpleMailMessage message = new SimpleMailMessage();
 
-        for (user oneUser : users) {
-            String to = oneUser.getEmail();
-            message.setTo(to);
-            message.setFrom("codingfriend7@gmail.com");
-            message.setSubject("서울 거주자 이벤트!");
-            message.setText("서울 거주자 안내 메일입니다.");
+            for (user oneUser : users) {
+                String to = oneUser.getEmail();
+                message.setTo(to);
+                message.setFrom("codingfriend7@gmail.com");
+                message.setSubject("서울 거주자 이벤트!");
+                message.setText("서울 거주자 안내 메일입니다.");
 
-            emailSender.send(message);
-        }
-
-
+                emailSender.send(message);
+            }
+            return "이메일 전송 성공";
+        }).exceptionally(ex -> {
+            System.out.println(ex);
+            return "전송 실패";
+        });
+        msFuture.get();
+        return null;
     }
 }
+
+    // @Scheduled/@Async 적용 (0)
+
