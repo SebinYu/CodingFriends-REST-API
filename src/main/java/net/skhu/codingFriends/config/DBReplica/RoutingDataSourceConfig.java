87694d3b827343cc62bd9f88f4e8491c09825ee8
SebinYu <sebinyu72@gmail.com>
2023-03-02 21:44:07 +0900

    refactor : DB master/slave 분리 - @Transactional(readOnly = true)로 기능 분리

---
+++ src/main/java/net/skhu/codingFriends/config/DBReplica/RoutingDataSourceConfig.java
@@ -0,0 +1,36 @@
+package net.skhu.codingFriends.config.DBReplica;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RoutingDataSourceConfig {
+
+    @Bean(name = "routingDataSource")
+    public DataSource routingDataSource(
+            @Qualifier("masterDataSource") final DataSource masterDataSource,
+            @Qualifier("slaveDataSource") final DataSource slaveDataSource
+    ) {
+        ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+
+        dataSourceMap.put(DataSourceType.Master, masterDataSource);
+        dataSourceMap.put(DataSourceType.Slave, slaveDataSource);
+
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
